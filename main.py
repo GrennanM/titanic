c8c06acfb7c91432ada765857d44e3436f150450
Mark Grennan <grennama@tcd.ie>
2018-12-01 21:17:28 +0000

    "updated pre-processing"

--- main.py
+++ main.py
@@ -7,13 +7,30 @@ def main():
     data = pd.read_csv(dataset, encoding='latin-1')
 
     # drop columns: Name, ID,
-    data.drop(columns = ['Name', 'PassengerId'], inplace = True)
+    data.drop(columns = ['Name', 'PassengerId', 'Cabin'], inplace = True)
 
     # change males = 1, females = 0
     data['Sex'] = data['Sex'].map( {'male':1, 'female':0} )
 
-    # data.info()
+    # change
+
+    # count the number of missing values
+    # print (data.isnull().sum())
+
+    # impute mean in place of missing values
+    data['Age'].fillna(data['Age'].mean(), inplace=True)
+
+    # drop 2 missing values from Embarked
+    data.dropna(subset=['Embarked'], inplace=True)
+
+    # print first few rows in data
     print(data.head())
+
+    # prints data types
+    data.info()
+
+    # describes data
+    print(data.describe())
 
 if __name__=='__main__':
     main()
