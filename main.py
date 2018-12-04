7a1e01588def2d03af77386dbd4019f60674c7ca
Mark Grennan <grennama@tcd.ie>
2018-12-04 21:19:29 +0000

    "factored out pre-processing"

--- main.py
+++ main.py
@@ -2,15 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 
-def main():
-    ############### PRE-PROCESSING ################################
-    dataset = '/home/markg/kaggle/titanic/dataset/titanicTrain.csv'
-    data = pd.read_csv(dataset, encoding='latin-1')
-
-    # label encoder
-    le = preprocessing.LabelEncoder()
-
-    #### MISSING DATA ######
+def preprocess(data):
     # count the number of missing values
     # print (data.isnull().sum())
 
@@ -20,30 +12,37 @@ def main():
     # drop 2 missing values from Embarked
     data.dropna(subset=['Embarked'], inplace=True)
 
-    # drop columns: Name, ID,
-    data.drop(columns = ['Name', 'PassengerId', 'Cabin'], inplace = True)
+    # drop columns: Name, ID, Cabin, Ticket
+    data.drop(columns = ['Name', 'PassengerId', 'Cabin', 'Ticket'],
+     inplace = True)
 
     # change males = 1, females = 0
     data['Sex'] = data['Sex'].map( {'male':1, 'female':0} )
-
-    # encode categorical variable 'Embarked'
-    # data['Embarked'] = le.fit_transform(data['Embarked'])
 
     # create n-1 dummy variables for 'Embarked' variable
     data = pd.get_dummies(data, columns=['Embarked'], prefix=['embark'],
      drop_first=True).head()
 
+    # Alternative method of encoding using sklearn
+    # encode categorical variable 'Embarked'
+    # data['Embarked'] = le.fit_transform(data['Embarked'])
+
     # to view encoded categories
     # print (list(le.classes_))
 
-    # print first few rows in data
+    return data
+
+
+def main():
+
+    dataset = '/home/markg/kaggle/titanic/dataset/titanicTrain.csv'
+    data = pd.read_csv(dataset, encoding='latin-1')
+
+    data = preprocess(data)
+
+    # print first few rows in data and data types
     print(data.head())
-
-    # prints data types
     print (data.info())
-
-    # describes data
-    # print(data.describe())
 
 if __name__=='__main__':
     main()
