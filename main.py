96606e8013d8b4496d689fe65ba9ffc8c5b98a4e
Mark Grennan <grennama@tcd.ie>
2018-12-04 21:04:53 +0000

    "Encode Embarked variable"

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import pandas as pd
 import numpy as np
+from sklearn import preprocessing
 
 def main():
     ############### PRE-PROCESSING ################################
     dataset = '/home/markg/kaggle/titanic/dataset/titanicTrain.csv'
     data = pd.read_csv(dataset, encoding='latin-1')
 
-    # drop columns: Name, ID,
-    data.drop(columns = ['Name', 'PassengerId', 'Cabin'], inplace = True)
+    # label encoder
+    le = preprocessing.LabelEncoder()
 
-    # change males = 1, females = 0
-    data['Sex'] = data['Sex'].map( {'male':1, 'female':0} )
-
-    # change
-
+    #### MISSING DATA ######
     # count the number of missing values
     # print (data.isnull().sum())
 
@@ -23,14 +20,30 @@ def main():
     # drop 2 missing values from Embarked
     data.dropna(subset=['Embarked'], inplace=True)
 
+    # drop columns: Name, ID,
+    data.drop(columns = ['Name', 'PassengerId', 'Cabin'], inplace = True)
+
+    # change males = 1, females = 0
+    data['Sex'] = data['Sex'].map( {'male':1, 'female':0} )
+
+    # encode categorical variable 'Embarked'
+    # data['Embarked'] = le.fit_transform(data['Embarked'])
+
+    # create n-1 dummy variables for 'Embarked' variable
+    data = pd.get_dummies(data, columns=['Embarked'], prefix=['embark'],
+     drop_first=True).head()
+
+    # to view encoded categories
+    # print (list(le.classes_))
+
     # print first few rows in data
     print(data.head())
 
     # prints data types
-    data.info()
+    print (data.info())
 
     # describes data
-    print(data.describe())
+    # print(data.describe())
 
 if __name__=='__main__':
     main()
