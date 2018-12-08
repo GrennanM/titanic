16ae5d0a4a3c50e45fd8678107b9ca11da5501cd
Mark Grennan <grennama@tcd.ie>
2018-12-08 17:57:59 +0000

    Factored out pre-processing and put clean data in a seperate file.

--- main.py
+++ main.py
@@ -2,66 +2,23 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
-
-def preprocess(data):
-    # count the number of missing values
-    # print (data.isnull().sum())
-
-    # impute mean in place of missing values
-    data['Age'].fillna(data['Age'].mean(), inplace=True)
-
-    # drop 2 missing values from Embarked
-    data.dropna(subset=['Embarked'], inplace=True)
-
-    # drop columns: Name, ID, Cabin, Ticket
-    data.drop(columns = ['Name', 'PassengerId', 'Cabin', 'Ticket'],
-     inplace = True)
-
-    # change males = 1, females = 0
-    data['Sex'] = data['Sex'].map( {'male':1, 'female':0} )
-
-    # create n-1 dummy variables for 'Embarked' variable
-    data = pd.get_dummies(data, columns=['Embarked'], prefix=['embark'],
-     drop_first=True)
-
-    # topcode Fare variable at 100
-    for i in range(0, len(data['Fare'])):
-        try:
-            if int(data['Fare'][i]) >= 100:
-                data.at[i, 'Fare'] = 100
-        except KeyError:
-            pass
-            # print ("KeyError Caught")
-
-     # histogram of Fare
-    # data['Fare'].plot.hist(grid=True, rwidth=0.9, color='#607c8e')
-    # plt.show()
-
-    # standardize numeric variables Age and Fare
-    numeric = ['Age', 'Fare']
-    data[numeric] = preprocessing.StandardScaler().fit_transform(data[numeric])
-
-    # Alternative method of encoding using sklearn
-    # encode categorical variable 'Embarked'
-    # data['Embarked'] = le.fit_transform(data['Embarked'])
-
-    # to view encoded categories
-    # print (list(le.classes_))
-
-    return data
-
+from sklearn.model_selection import train_test_split
 
 def main():
 
-    dataset = '/home/markg/kaggle/titanic/dataset/titanicTrain.csv'
-    data = pd.read_csv(dataset, encoding='latin-1')
-    df = preprocess(data)
+    dataset = '/home/markg/kaggle/titanic/titanicClean.csv'
+    df = pd.read_csv(dataset, encoding='latin-1')
+    df = df.drop(columns = ['Unnamed: 0']) # identifier column
 
     # print first few rows in data and data types
-    # print(df.head())
+    print(df.head())
     # print (df.describe())
-    print (df.info())
+    # print (df.info())
 
+    # split into 90% training, 10% testing
+    X = df.drop(columns = ['Survived'])
+    Y = df['Survived']
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.10)
 
 if __name__=='__main__':
     main()
